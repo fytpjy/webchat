37dfdcaf26907dcfc081ddf191f9300573405c67
fengyt <fengyt@youshu.cc>
2017-10-31 21:00:40 +0800

    页面

--- config.js
+++ config.js
@@ -1,8 +1,5 @@
 module.exports = {
     /** 通讯域名 */
-    host: 'https://wp.youshu.cc/',
-    // host: 'http://api.fengwo.com:81/',
-    // host: 'https://wptest.youshu.cc/',
     // basePath: '/api/wishlist/',
     basePath: '',
 };
